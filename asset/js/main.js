c7422d4388405be7a46a7aca4bd8bc76853c4faa
DarioConiglione <dariocng13@gmail.com>
2025-10-16 13:12:31 +0100

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,6 @@
+// GENERARE IN PAGINA 5 NUMERI CASUALI - Utilizzare mathRandom e creare una variabile in main e stamparla in index
+// IMPOSTARE UN TIMER DI 30 SECONDI AFFINCHE POI SPARISCANO (la variabile sopra deve stare dentro la funzione).
+// CHIEDERE ALL'UTENTE DI INSERIRE 5 NUMERI (tramite prompt o sumbit)
+// INSERIRE IN UNA ARRAY I CINQUE NUMERI CASUALI
+// INSERIRE IN UNA ARRAY I CINQUE NUMERI SCELTI DALL'USER
+// PARAGONARE LE DUE ARRAY
