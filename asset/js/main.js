58023f122a088b936875949b381fa38f6f33de2e
DarioConiglione <dariocng13@gmail.com>
2025-10-16 13:48:06 +0100

    Create 5 random numbers

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,6 +1,29 @@
 // GENERARE IN PAGINA 5 NUMERI CASUALI - Utilizzare mathRandom e creare una variabile in main e stamparla in index
+// INSERIRE IN UNA ARRAY I CINQUE NUMERI CASUALI
+let cpuNumber = []
+
+for (let i = 0; i < 5; i++) {
+    let numRandom = Math.floor(Math.random() * 100);
+  cpuNumber.push(numRandom);
+}
+
+console.log(cpuNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // IMPOSTARE UN TIMER DI 30 SECONDI AFFINCHE POI SPARISCANO (la variabile sopra deve stare dentro la funzione).
 // CHIEDERE ALL'UTENTE DI INSERIRE 5 NUMERI (tramite prompt o sumbit)
-// INSERIRE IN UNA ARRAY I CINQUE NUMERI CASUALI
+
 // INSERIRE IN UNA ARRAY I CINQUE NUMERI SCELTI DALL'USER
 // PARAGONARE LE DUE ARRAY
