cc14fa9b077fa404e3236631aa91d273cddd4f2f
DarioConiglione <dariocng13@gmail.com>
2025-10-16 17:09:44 +0100

    compare two array

--- asset/js/main.js
+++ asset/js/main.js
@@ -13,7 +13,7 @@ const firstFiveNumEL = document.getElementById("firstFiveNum");
 firstFiveNumEL.innerText = cpuNumber;
 
 // IMPOSTARE UN TIMER DI 30 SECONDI AFFINCHE POI SPARISCANO 
-setTimeout(TenSeconds, 5000);
+setTimeout(TenSeconds, 3000);
 
 function TenSeconds() {
     firstFiveNumEL.innerText = "";
@@ -22,16 +22,39 @@ function TenSeconds() {
 
 // CHIEDERE ALL'UTENTE DI INSERIRE 5 NUMERI (tramite prompt o sumbit)
 let userNumber = []
-const formEl = document.querySelector(".form-label");
+const formEl = document.querySelector(".form");
+const inputs = formEl.querySelectorAll("input");
 
 formEl.addEventListener('submit', (event) => {
-
-    userNumber.push(formEl.value);
-    console.log(userNumber);
     event.preventDefault();
+
+    inputs.forEach(input => {
+        userNumber.push(input.value);
+    });
+
+
+
+    for (let i = 0; i < cpuNumber.length; i++) {
+        for (let u = 0; u < userNumber.length; u++){
+            if (cpuNumber[i] == userNumber[i]) {
+                console.log("identico");
+                
+            } else {
+                console.log("diverso");
+                
+            }
+        }
+    }
+
+
+
+    console.log(userNumber);
 });
 
 
-
-// INSERIRE IN UNA ARRAY I CINQUE NUMERI SCELTI DALL'USER
 // PARAGONARE LE DUE ARRAY
+
+
+
+
+
